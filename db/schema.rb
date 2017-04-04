a8a3b59ddde6745c940da6c12dd8347289b3e217
huy <huy@zoyi.co>
2017-04-05 02:55:07 +0900

    Remove trash 2.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,4 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170403082447) do
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-  end
-
 end
