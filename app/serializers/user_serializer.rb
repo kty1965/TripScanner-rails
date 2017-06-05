4b5b3113f489740c98921d943ad20d73716f6981
huy <huy@zoyi.co>
2017-06-05 16:54:27 +0900

    Add reviews.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :school, :job, :locale, :country,
     :image_thumb, :image_medium, :image_original, :introduction,
     :written_reviews_count, :written_reviews_rate
+
+  has_many :reviews
 end
