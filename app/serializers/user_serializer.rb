c75c17cd1c46c96f3dec775e5f1dc1b25c3661d0
huy <huy@zoyi.co>
2017-06-05 17:00:23 +0900

    Change reviews to owned_reviews_written.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :school, :job, :locale, :country,
     :image_thumb, :image_medium, :image_original, :introduction,
-    :written_reviews_count, :written_reviews_rate
+    :written_reviews_count, :written_reviews_rate, :owned_reviews_written
 
-  has_many :reviews
+  def owned_reviews_written
+    object.owned_reviews.written
+  end
 end
